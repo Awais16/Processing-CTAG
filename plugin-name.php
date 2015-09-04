36e5dc58a65a819ceeb4c23c73c67bd76fb97e82
Awais <owasii@gmail.com>
2015-09-04 10:02:31 +0200

    pluging for processing js

---
+++ plugin-name.php
@@ -0,0 +1,77 @@
+<?php
+
+/**
+ * The plugin bootstrap file
+ *
+ * This file is read by WordPress to generate the plugin information in the plugin
+ * admin area. This file also includes all of the dependencies used by the plugin,
+ * registers the activation and deactivation functions, and defines a function
+ * that starts the plugin.
+ *
+ * @link              http://example.com
+ * @since             1.0.0
+ * @package           Plugin_Name
+ *
+ * @wordpress-plugin
+ * Plugin Name:       CTAG-ProcessingJs
+ * Plugin URI:        https://github.com/Awais16/Processing-CTAG
+ * Description:       For showcasing processingJs example in browser
+ * Version:           1.0.0
+ * Author:            Awais
+ * Author URI:        http://example.com/
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       CTAG-ProcessingJs
+ * Domain Path:       /languages
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+define("CTAG_BASE_URL",plugin_dir_url( __FILE__ ));
+
+/**
+ * The code that runs during plugin activation.
+ * This action is documented in includes/class-plugin-name-activator.php
+ */
+function activate_ctag_processing() {
+	require_once plugin_dir_path( __FILE__ ) . 'includes/class-plugin-name-activator.php';
+	Plugin_Name_Activator::activate();
+}
+
+/**
+ * The code that runs during plugin deactivation.
+ * This action is documented in includes/class-plugin-name-deactivator.php
+ */
+function deactivate_ctag_processing() {
+	require_once plugin_dir_path( __FILE__ ) . 'includes/class-plugin-name-deactivator.php';
+	Plugin_Name_Deactivator::deactivate();
+}
+
+register_activation_hook( __FILE__, 'activate_ctag_processing' );
+register_deactivation_hook( __FILE__, 'deactivate_ctag_processing' );
+
+/**
+ * The core plugin class that is used to define internationalization,
+ * admin-specific hooks, and public-facing site hooks.
+ */
+require plugin_dir_path( __FILE__ ) . 'includes/class-plugin-name.php';
+
+/**
+ * Begins execution of the plugin.
+ *
+ * Since everything within the plugin is registered via hooks,
+ * then kicking off the plugin from this point in the file does
+ * not affect the page life cycle.
+ *
+ * @since    1.0.0
+ */
+function run_plugin_name() {
+
+	$plugin = new Plugin_Name();
+	$plugin->run();
+
+}
+run_plugin_name();
